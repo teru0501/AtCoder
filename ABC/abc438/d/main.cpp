97cfbad9e2731eac7431fc82c1061e64d9f6ead0
tery0501 <teru12310501@gmail.com>
2025-12-27 23:01:03 +0900

    add

---
+++ ABC/abc438/d/main.cpp
@@ -0,0 +1,9 @@
+// abc438 D - Tail of Snake 
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+int main() {
+  
+}
---
+++ ABC/abc438/f/main.cpp
@@ -0,0 +1,9 @@
+// abc438 F - Sum of Mex
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+int main() {
+  
+}
