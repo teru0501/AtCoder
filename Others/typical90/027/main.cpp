8bf05d87220d992f98f9b8d977d986c81799c814
tery0501 <teru12310501@gmail.com>
2026-01-10 17:59:12 +0900

    1/10

--- Others/typical90/027/main.cpp
+++ Others/typical90/027/main.cpp
@@ -4,71 +4,71 @@
 using namespace std;
 using namespace atcoder;
 
-#pragma region TYPE
+#pragma region TEMPLATE
+// ================= TYPE ================= //
 using ll = long long;
 using ld = long double;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-using pci = pair<char, int>;
-using pic = pair<int, char>;
-using psi = pair<string, int>;
-using pis = pair<int, string>;
 using vi = vector<int>;
 using vll = vector<ll>;
-using vs = vector<string>;
-using vc = vector<char>;
 using vvi = vector<vector<int>>;
 using vvll = vector<vector<ll>>;
-using vvs = vector<vector<string>>;
-using vvc = vector<vector<char>>;
-#pragma endregion
-#pragma region MACRO
+
+// ================= MACRO ================= //
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
 #define YES cout << "Yes" << endl
 #define NO cout << "No" << endl
 #define IN(v) for (auto &x : (v)) cin >> x;
 #define OUT(x) cout << (x) << endl
-#define rep0(i,n) for(int i = 0; i < (n); ++i)
-#define rep1(i,n) for(int i = 1; i <= (n); ++i)
-#define rep_range(i, l, r) for (int i = (l); i < (r); ++i)
-#pragma endregion
-#pragma region CONST
+#define VOUT(v) do { for(auto x : (v)) cout << x << " "; cout << endl; } while(0)
+#define VVOUT(vv) do { for(auto &v : (vv)) VOUT(v); } while(0)
+#define rep0(i,n) for(ll i = 0; i < (n); i++)
+#define rep1(i,n) for(ll i = 1; i <= (n); i++)
+
+// ================= CONST ================= //
 const vi dx = {-1,0,1,0};
 const vi dy = {0,1,0,-1};
 //const vi dx = {-1,-1,-1,0,1,1,1,0};
-//const vi dx = {-1,0,1,1,1,0,-1,-1};
-const int INF = INT16_MAX;
-const ll LINF = INT32_MAX;
+//const vi dy = {-1,0,1,1,1,0,-1,-1};
+const int INF = 1e9;
+const ll LINF = (ll)4e18;
 const int MOD = 998244353;
-#pragma endregion
-#pragma region UTILITY
-bool out_grid(ll i, ll j, ll h, ll w) {
-    return (!(0 <= i && i < h && 0 <= j && j < w));
-}
-template <class T>
-void VOUT(const vector<T>& v) {
-    for (int i = 0; i < (int)v.size(); ++i) {
-        if (i) cout << " ";
-        cout << v[i];
-    }
-    cout << endl;
-}
-template <class T>
-void VVOUT(const vector<vector<T>>& vv) {
-    for (const auto& v : vv) {
-        VOUT(v);
-    }
-}
+
+// ================= UTILITY ================= //
+bool in_grid(ll i, ll j, ll h, ll w) {return(0 <= i && i < h && 0 <= j && j < w);}
 template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
 template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
 #pragma endregion
+
+// ================= STRATEGY ================= //
+// 制約をみろ！！
+// 愚直を考えろ！！
+// オバフロ注意！！
+// ============================================ //
+
+void solve () {
+  int n;
+  cin >> n;
+  unordered_set<string> st;
+  rep1 (i, n) {
+    string s;
+    cin >> s;
+    if (!st.count(s)) {
+        OUT(i);
+        st.insert(s);
+    }
+  }
+  return;
+}
 
 // ---------------------- main ----------------------
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
   cout << setprecision(12) << fixed;
-  
+  solve();
   return 0;
 }
