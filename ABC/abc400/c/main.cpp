2463fc11e4a94717abf9d81271042486e58032b6
tery0501 <teru12310501@gmail.com>
2026-01-16 18:20:50 +0900

    add

--- ABC/abc400/c/main.cpp
+++ ABC/abc400/c/main.cpp
@@ -1,95 +1,78 @@
 // abc400 C - 2^a b^2
 #include <bits/stdc++.h>
 #include <atcoder/all>
+#include <math.h>
 using namespace std;
 using namespace atcoder;
 
+#pragma region TEMPLATE
+// ================= TYPE ================= //
 using ll = long long;
 using ld = long double;
-
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-
 using vi = vector<int>;
 using vll = vector<ll>;
-
 using vvi = vector<vector<int>>;
 using vvll = vector<vector<ll>>;
 
+// ================= MACRO ================= //
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
 #define YES cout << "Yes" << endl
 #define NO cout << "No" << endl
 #define IN(v) for (auto &x : (v)) cin >> x;
 #define OUT(x) cout << (x) << endl
-#define rep1(i,n) for(int i = 0; i < (n); ++i)
-#define rep2(i,n) for(int i = 1; i <= (n); ++i)
+#define VOUT(v) do { for(auto x : (v)) cout << x << " "; cout << endl; } while(0)
+#define VVOUT(vv) do { for(auto &v : (vv)) VOUT(v); } while(0)
+#define rep0(i,n) for(ll i = 0; i < (n); i++)
+#define rep1(i,n) for(ll i = 1; i <= (n); i++)
 
+// ================= CONST ================= //
 const vi dx = {-1,0,1,0};
 const vi dy = {0,1,0,-1};
 //const vi dx = {-1,-1,-1,0,1,1,1,0};
-//const vi dx = {-1,0,1,1,1,0,-1,-1};
+//const vi dy = {-1,0,1,1,1,0,-1,-1};
+const int INF = 1e9;
+const ll LINF = (ll)4e18;
+const int MOD = 998244353;
 
-bool out_grid(ll i, ll j, ll h, ll w) {
-    return (!(0 <= i && i < h && 0 <= j && j < w));
-}
-
-template <class T>
-void VOUT(const vector<T>& v) {
-    for (int i = 0; i < (int)v.size(); ++i) {
-        if (i) cout << " ";
-        cout << v[i];
-    }
-    cout << endl;
-}
-
-template <class T>
-void VVOUT(const vector<vector<T>>& vv) {
-    for (const auto& v : vv) {
-        VOUT(v);
-    }
-}
-
+// ================= UTILITY ================= //
+bool in_grid(ll i, ll j, ll h, ll w) {return(0 <= i && i < h && 0 <= j && j < w);}
 template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
 template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+#pragma endregion
 
-//+2, *2, *2, +2になってる？
+// ================= STRATEGY ================= //
+// 制約をみろ！！
+// 愚直を考えろ！！
+// オバフロ注意！！
+// bを固定して*2していく
+// ============================================ //
+
+void solve () {
+  ll n;
+  cin >> n;
+  ll b = 1;
+  ll ans = 0;
+  while (b * b <= n) {
+    ll tmp = b * b;
+    while (tmp * 2 <= n) {
+        tmp *= 2;
+        ans++;
+    }
+    b += 2;
+  }
+  OUT(ans);
+  return;
+}
 
 // ---------------------- main ----------------------
 int main() {
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
   cout << setprecision(12) << fixed;
-  
-  ll n;
-  cin >> n;
-  ll ans = 0;
-  ll sum = 0;
-  int f = 0;
-  while (true) {
-    if (sum > n) {
-        OUT(ans);
-        return 0;
-    }
-    else {
-        ans++;
-    }
-    if (f == 0) {
-        sum += 2;
-        f = 1;
-    }
-    else if (f == 1) {
-        sum *= 2;
-        f = 2;
-    }
-    else if (f == 2) {
-        sum *= 2;
-        f = 3;
-    }
-    else if (f == 3) {
-        sum *= 2;
-        f = 0;
-    }
-  }
+  solve();
   return 0;
 }
---
+++ ARC/arc014/b/main.cpp
@@ -0,0 +1,86 @@
+// arc014 B - あの日したしりとりの結果を僕達はまだ知らない。
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+#pragma region TEMPLATE
+// ================= TYPE ================= //
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+// ================= MACRO ================= //
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define IN(v) for (auto &x : (v)) cin >> x;
+#define OUT(x) cout << (x) << endl
+#define VOUT(v) do { for(auto x : (v)) cout << x << " "; cout << endl; } while(0)
+#define VVOUT(vv) do { for(auto &v : (vv)) VOUT(v); } while(0)
+#define rep0(i,n) for(ll i = 0; i < (n); i++)
+#define rep1(i,n) for(ll i = 1; i <= (n); i++)
+
+// ================= CONST ================= //
+const vi dx = {-1,0,1,0};
+const vi dy = {0,1,0,-1};
+//const vi dx = {-1,-1,-1,0,1,1,1,0};
+//const vi dy = {-1,0,1,1,1,0,-1,-1};
+const int INF = 1e9;
+const ll LINF = (ll)4e18;
+const int MOD = 998244353;
+
+// ================= UTILITY ================= //
+bool in_grid(ll i, ll j, ll h, ll w) {return(0 <= i && i < h && 0 <= j && j < w);}
+template<typename T> bool chmin(T& a, T b){if(a > b){a = b; return true;} return false;}
+template<typename T> bool chmax(T& a, T b){if(a < b){a = b; return true;} return false;}
+#pragma endregion
+
+// ================= STRATEGY ================= //
+// 制約をみろ！！
+// 愚直を考えろ！！
+// オバフロ注意！！
+// setで重複管理
+// charで最後の文字を管理
+// boolで今誰の番であるかを管理
+// - 0 高橋くん
+// - 1 高橋クン
+// ============================================ //
+
+void solve () {
+  int n;
+  cin >> n;
+  bool f = 0;
+  char c = '0';
+  unordered_set<string> s;
+  rep0 (i, n) {
+    string tmp;
+    cin >> tmp;
+    if ((c == '0' || c == tmp[0]) && !s.count(tmp)) f = !f;
+    else {
+      if (f) OUT("WIN");
+      else OUT("LOSE");
+      return;
+    }
+    s.insert(tmp);
+    c = tmp[tmp.size() - 1];
+  }
+  OUT("DRAW");
+  return;
+}
+
+// ---------------------- main ----------------------
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(12) << fixed;
+  solve();
+  return 0;
+}
